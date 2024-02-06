4a81b072b067024995361cc450213d013c4499f8
jonathanameri <jonathan.ameri@uconn.edu>
2024-02-06 13:38:58 -0500

    flask app with uploading capability and success page

--- hw03/main.py
+++ hw03/main.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from distutils.log import debug
+from fileinput import filename
+from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    return render_template('index.html')
+
+@app.route('/success',methods = ['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        return render_template("acknowledgement.html", name = f.filename)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
