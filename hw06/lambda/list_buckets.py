d0e3e15a84bec429d288194a5ebc138cba04d5f4
jonathanameri <jonathan.ameri@uconn.edu>
2024-02-27 15:55:08 -0500

    list_buckets use s3 client

--- hw06/lambda/list_buckets.py
+++ hw06/lambda/list_buckets.py
@@ -1,9 +1,14 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
-    # TODO implement
-    print('test')
+
+    print('list_buckets. event:', event)
+
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(response)
     }
