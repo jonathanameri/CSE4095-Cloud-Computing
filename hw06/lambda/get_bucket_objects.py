3901e11919ce51db0f07281fc1f4e874ed0d8d61
jonathanameri <jonathan.ameri@uconn.edu>
2024-02-27 16:46:50 -0500

    get_bucket_objects added functionality

--- hw06/lambda/get_bucket_objects.py
+++ hw06/lambda/get_bucket_objects.py
@@ -1,10 +1,20 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
     bucket_name = event["bucket"]
+
     print('bucket_name:', bucket_name)
+    s3 = boto3.client('s3')
+    response = s3.list_objects_v2(Bucket=bucket_name)
+
+    print('response:', response)
+
+    object_list = []
+    for obj in response['Contents']:
+        object_list.append(obj['Key'])
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(object_list)
     }
